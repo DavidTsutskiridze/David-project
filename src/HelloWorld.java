37e10d7d50fa54fed8a9a490893513ecab4a31d6
David Tsutskiridze <davidtsutskiridze@Davids-MacBook-Air.local>
2021-01-23 15:17:14 -0500

    nice car

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
 
+        // git add
+        // git status
+        // git commit -m "Hello World"
+        // git push
+
     }
 }
